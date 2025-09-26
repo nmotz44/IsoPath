bf94410aa87523a58d8d0c3e8673ca5063afa336
Noah Motz <nmotz@udel.edu>
2025-09-26 15:59:42 -0400

    new manual, still need to fix the .rda data

--- R/data.R
+++ R/data.R
@@ -6,15 +6,14 @@
 #' \describe{
 #'   \item{d13C}{Carbon isotope composition (numeric).}
 #'   \item{d15N}{Nitrogen isotope composition (numeric).}
-#'   \item{species}{Species name with classification (character).}
-#'   \item{species2}{Simplified species name (character).}
-#'   \item{Ecopath_Group}{Ecopath model group classification (character).}
 #'   \item{Functional_Group}{Functional group classification (character).}
 #'   \item{source}{Source of the measurement (character).}
 #'   \item{note}{Additional notes (character).}
 #' }
 #' @source data from experimental design
 #'
-#' @example data(AFS_SIA_Data)
+#' @examples
+#' # example code
+#'  data(AFS_SIA_Data)
 
 "AFS_SIA_Data"
